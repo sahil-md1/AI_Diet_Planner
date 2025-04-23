9804741da3dfc73b822260e756f8c59db001b8fd
md sahil <ms.mdsahil77@gmail.com>
2025-04-24 00:51:06 +0530

    changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -67,8 +67,8 @@ const styles = StyleSheet.create({
   reactLogo: {
     height: 178,
     width: 290,
-    // bottom: 0,
-    // left: 0,
+    bottom: 0,
+    left: 0,
     position: 'absolute',
   },
 });
